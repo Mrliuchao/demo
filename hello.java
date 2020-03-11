70a3f0981c92361a607afd49c79cf2ac528212f9
刘超 <57904936+Mrliuchao@users.noreply.github.com>
2020-03-11 11:19:45 +0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-<<<<<<< HEAD
     System.out.println("1111111111111111111111111111");
-=======
->>>>>>> 23974e5613748cfcfd49cdaa622a4b5b92c834d2
     }
 }
